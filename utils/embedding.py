d830c93b2bf1bf6d79b3472faee5c6863e32612a
amit <sandilyaamit1@gmail.com>
2025-06-11 19:21:06 +0530

    git commit for first time rag

---
+++ utils/embedding.py
@@ -0,0 +1,17 @@
+import requests
+import numpy as np
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("EURI_API_KEY")  # Use env var for security
+
+def get_embedding(text, model="text-embedding-3-small"):
+    url = "https://api.euron.one/api/v1/euri/alpha/embeddings"
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+    payload = {"input": text, "model": model}
+    response = requests.post(url, headers=headers, json=payload)
+    return np.array(response.json()['data'][0]['embedding'])
